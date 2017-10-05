53d55fbef00d77ea0a4b9468d634d174b475e0e4
Kelrynn <cameroncastells@gmail.com>
2017-10-05 08:35:03 -0600

    Routing information

--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-router = express.Router();
+var router = express.Router();
 
-var candies;
+var candies = [
+	{"id":1,"name":"Chewing Gum","color":"Red"},
+	{"id":2,"name":"Pez","color":"Green"},
+	{"id":3,"name":"Marshmallow","color":"Pink"},
+	{"id":4,"name":"Candy Stick","color":"Blue"}
+];
 
 //What would need to go into candies
 //in order to pass our first test?
@@ -10,8 +15,57 @@ var candies;
 router.get('/', function(req,res) {
 	// What would go here? 
 	// Hint: we want all candies in JSON format
+	res.json(candies);
 });
 
 // Fill out the rest of the routes here
 
+router.get('/:id', (req,res) => {
+	var myCandy = candies.filter((element) => {
+		return element.id == req.params.id;
+	});
+	res.json(myCandy[0]);
+});
+
+router.post('/', (req, res) => {
+	candies.push(req.body);
+	res.send("Candy saved!");
+});
+
+router.put('/:id',(req, res) => {
+	var myCandy = candies.filter((element) => {
+		return element.id == req.params.id;
+	});
+	var index = candies.indexOf(myCandy[0]);
+	candies[index] = req.body;
+	res.send("Candy Updated!");
+});
+
+router.delete('/:id', (req, res) => {
+	var myCandy = candies.filter((element) => {
+		return element.id == req.params.id;
+	});
+	var index = candies.indexOf(myCandy[0]);
+	candies.splice(index, 1);
+	res.send("Candy Deleted");
+});
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
